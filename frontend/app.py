ed12613244bdbe1697b849df4cf20e990baae60a
feriadiputra-eng <feriadiputraall@gmail.com>
2025-06-21 09:09:46 +0700

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = 'ini_rahasia_kelompok2'  # digunakan untuk flash message
 
 # Ganti ini dengan URL endpoint Lambda API kamu
-API_BASE = 'https://ncqpvuxxng.execute-api.us-east-1.amazonaws.com/dev/users'
+API_BASE = 'https://h1e6dv0k00.execute-api.us-east-1.amazonaws.com/dev/users'
 
 @app.route('/')
 def index():
